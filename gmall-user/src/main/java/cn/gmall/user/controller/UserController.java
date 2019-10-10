9b98caf0087a62aea6b9b1919b4ad535c5ee80aa
Administrator <704645300>
2019-10-10 20:57:03 +0800

    第2次

---
+++ gmall-user/src/main/java/cn/gmall/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package cn.gmall.user.controller;
+
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserService userService;
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses=userService.getReceiveAddressByMemberId(memberId);
+        return umsMemberReceiveAddresses;
+    }
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers=userService.getAllUser();
+        return umsMembers;
+    }
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+}
---
+++ gmall-user/src/main/java/cn/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,7 @@
+package cn.gmall.user.mapper;
+
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+}
