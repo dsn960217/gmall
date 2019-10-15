a6c1c10efa380907a16fdb2b8b6dbc17bf789804
Administrator <704645300>
2019-10-15 20:41:27 +0800

    第7次

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
@@ -2,6 +2,9 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.bean.PmsBaseSaleAttr;
+import com.atguigu.gmall.bean.PmsProductSaleAttr;
 import com.atguigu.gmall.service.AttrService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +18,15 @@ import java.util.List;
 public class AttrController {
     @Reference
     AttrService attrService;
+
+
+    @RequestMapping("baseSaleAttrList")
+    @ResponseBody
+    public List<PmsBaseSaleAttr> baseSaleAttrList(){
+
+        List<PmsBaseSaleAttr> pmsBaseSaleAttrs=attrService.baseSaleAttrList();
+        return  pmsBaseSaleAttrs;
+    }
     @RequestMapping("attrInfoList")
     @ResponseBody
     public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id){
@@ -28,4 +40,11 @@ public class AttrController {
         return "sucess";
     }
 
+    @RequestMapping("getAttrValueList")
+    @ResponseBody
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId){
+        List<PmsBaseAttrValue> pmsBaseAttrValues=attrService.getAttrValueList(attrId);
+
+        return pmsBaseAttrValues;
+    }
 }
---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -0,0 +1,36 @@
+package com.atguigu.gmall.manage.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.service.SpuService;
+import org.apache.tomcat.jni.Multicast;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Controller
+@CrossOrigin
+public class SpuController {
+    @Reference
+    SpuService spuService;
+
+    @RequestMapping("foleUpload")
+    @ResponseBody
+    public String foleUpload(@RequestParam("file")MultipartFile multipartFile){
+    return "success";
+    }
+    @RequestMapping("saveSpuInfo")
+    @ResponseBody
+    public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+
+        return "success";
+    }
+    @RequestMapping("spuList")
+    @ResponseBody
+    public List<PmsProductInfo> spuList(String catalog3Id){
+        List<PmsProductInfo> pmsProductInfos=spuService.spuList(catalog3Id);
+        return pmsProductInfos;
+    }
+}
