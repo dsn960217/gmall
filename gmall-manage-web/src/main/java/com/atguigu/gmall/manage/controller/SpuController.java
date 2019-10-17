d379ddb15c66c7acc73499cb69725e8a28554b60
Administrator <704645300>
2019-10-17 19:58:50 +0800

    第8次

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.manage.util.PmsUploadUtil;
 import com.atguigu.gmall.service.SpuService;
 import org.apache.tomcat.jni.Multicast;
 import org.springframework.stereotype.Controller;
@@ -19,7 +20,8 @@ public class SpuController {
     @RequestMapping("foleUpload")
     @ResponseBody
     public String foleUpload(@RequestParam("file")MultipartFile multipartFile){
-    String imgUrl="";
+    String imgUrl= PmsUploadUtil.uploadImage(multipartFile);
+        System.out.println(imgUrl);
     return imgUrl;
     }
     @RequestMapping("saveSpuInfo")
